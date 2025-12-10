aff8ce4014f09f139804fc0ff7ca6300f3221ef2
itsyigido <ygtyasar06@gmail.com>
2025-12-10 20:02:33 +0300

    Update

---
+++ ders35-36/DikdortgenCiz.java
@@ -0,0 +1,11 @@
+public class DikdortgenCiz {
+      public static void main(String[] var0) {
+        for (int j = 0; j < 5; j++) {
+            for(int i = 0; i < j; i++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        } 
+}
+}
